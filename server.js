8d0a786ed8527c8eb760de344d0a8273380abc7c
denizmslm <128998242+denizmslm@users.noreply.github.com>
2025-02-12 17:17:30 +0300

    added node server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+const basePath = __dirname + "/src/pages";
+
+// "public" klasörünü statik hale getir (Tüm HTML, CSS, JS dosyaları burada olacak)
+app.use(express.static("src"));
+
+// Ana sayfa (index.html)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(basePath, "/index.html"));
+});
+
+// About
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(basePath, "/about.html"));
+});
+
+// Sunucuyu başlat
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
